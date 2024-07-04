0b4a6e60839aaffc2595cc766a17c4a6d3a4f2e5
neket <nikitaivanovitc@gmail.com>
2024-07-04 14:01:50 +0300

    add tests on auth

--- PoteiTestCases/src/test/java/tests/BaseTest.java
+++ PoteiTestCases/src/test/java/tests/BaseTest.java
@@ -2,12 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class BaseTest {
 
     @BeforeSuite
     public void beforeSuite() {
-        Configuration.baseUrl ="http://192.168.250.26";
+        Configuration.baseUrl ="http://192.168.31.246";
         Configuration.browser ="chrome";
     }
+
 }
